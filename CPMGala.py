b0453bebdff81a2dd057a9abd8f44597d0f5c40f
CPMGalakz <cpmgala.tool@gmail.com>
2025-02-11 19:39:17 +0500

    Update and rename cpmelsedev.py to CPMGala.py

--- CPMGala.py
+++ CPMGala.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
-BASE_URL: str = "https://cpmelsedevshop.squareweb.app/api"
+BASE_URL: str = "https://maroon-echidna-796942.hostingersite.com/api"
 
-class CPMElsedev:
+class CarParkTool:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
